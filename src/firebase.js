712d3d597f14df3d11756715c7043f1efd15a85c
Kirill McQuillin <97107415+KirillMcQ@users.noreply.github.com>
2024-05-03 22:29:43 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyABSuHYhHsF3BKWmx8XHizGSCjgUSmGJFk',
-  authDomain: 'chatapp-39386.firebaseapp.com',
-  projectId: 'chatapp-39386',
-  storageBucket: 'chatapp-39386.appspot.com',
-  messagingSenderId: '1016853351986',
-  appId: '1:1016853351986:web:bbe29993f56acd1a0db62b',
-  measurementId: 'G-487E41R7Z4',
-};
+// firebase configuration removed for security reasons 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
